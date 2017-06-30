d5f6662cbf84c840da530fc9037072c419f06ab3
RandomAmbersky <random@random.ran>
2017-07-01 01:41:50 +0300

    first init

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import ThemeProvider from 'react-toolbox/lib/ThemeProvider';
+import theme from './toolbox/theme'
+
+import App from './App';
+import './index.css';
+import './toolbox/theme.css'
+
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+  <App />
+  </ThemeProvider>,
+  document.getElementById('root'));
+registerServiceWorker();
